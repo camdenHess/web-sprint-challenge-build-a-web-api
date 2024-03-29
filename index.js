28d60b7bc72935a11bae3d4dad5e496b735596f0
Camden Hess <camden.hess12@gmail.com>
2024-03-29 11:05:53 -0600

    made changes to better organize server

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 require('dotenv').config()
-const express = require('express')
-
-const server = express()
+const server = require('./api/server')
 
 const PORT = process.env.PORT || 9000
-
-server.use(express.json())
-
-
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
