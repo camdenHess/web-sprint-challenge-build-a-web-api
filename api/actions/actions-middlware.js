9cd0314c193e93d740e935c95ddcfa84de7a97a3
Camden Hess <camden.hess12@gmail.com>
2024-03-30 19:13:09 -0600

    added put and delete for actions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,2 +1,52 @@
 const Action = require('./actions-model')
 
+const validateActionsId = (req, res, next) => {
+    Action.get(req.params.id)
+        .then(action => {
+            if (!action) {
+                next({status: 404, message: 'action not found'})
+            } else {
+                req.action = action
+                next()
+            }
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: 'problem finding action'
+            })
+        })
+}
+
+const validateAction = (req, res, next) => {
+    const { project_id, description, notes, completed } = req.body
+    if (!project_id || !description || !notes) {
+        next({status: 400, message: 'project_id, description, and notes are required'})
+    } else {
+        if (completed) {
+            req.completed = completed
+        }
+        req.project_id = project_id
+        req.description = description
+        req.notes = notes
+        next()
+    }
+}
+
+const putValidateAction = (req, res, next) => {
+    const { project_id, description, notes, completed } = req.body
+    if (!project_id || !description || !notes || completed === undefined) {
+        next({status: 400, message: 'project_id, description, notes, and completed are required'})
+    } else {
+        req.comp = completed
+        req.project_id = project_id
+        req.description = description
+        req.notes = notes
+        next()
+    }
+}
+
+module.exports = {
+    validateActionsId,
+    validateAction,
+    putValidateAction
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-//middleware
+const { validateActionsId, validateAction, putValidateAction } = require('./actions-middlware')
 
 const Action = require('./actions-model')
 
@@ -14,20 +14,35 @@ router.get('/', (req, res, next) => {
         .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
-    console.log(req)
+router.get('/:id', validateActionsId, (req, res) => {
+    res.json(req.action)
 })
 
-router.post('/', (req, res, next) => {
-    console.log(req)
+router.post('/', validateAction, (req, res, next) => {
+    Action.insert({project_id: req.project_id, description: req.description, notes: req.notes, completed: req.completed})
+        .then(newAction => {
+            res.status(201).json(newAction)
+        })
+        .catch(next)
 })
 
-router.put('/:id', (req, res, next) => {
-    console.log(req)
+router.put('/:id', validateActionsId, putValidateAction, (req, res, next) => {
+    console.log(req.comp, req.project_id, req.description, req.notes)
+    Action.update(req.params.id, { project_id: req.project_id, description: req.description, notes: req.notes, completed: req.comp })
+        .then(updatedAction => {
+            res.json(updatedAction)
+        })
+        .catch(next)
 })
 
-router.delete('/:id', (req, res, next) => {
-    console.log(req)
+router.delete('/:id', validateActionsId, (req, res, next) => {
+    Action.remove(req.params.id)
+        .then(() => {
+            res.status(200).json({
+                message: 'successfully deleted action'
+            })
+        })
+        .catch(next)
 })
 
 
